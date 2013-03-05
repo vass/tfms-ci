d05f1b2944eb3fdfe38a7da2515def52ef04b821
Evan Scown <sir.lagalot.thegreat@gmail.com>
2013-03-05 23:33:41 +1000

    Initial Commit

---
+++ tfms/core/TFMS_Controller.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class TFMS_Controller extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+}
+
+
+/* End of file TFMS_Controller.php */
+/* Location: ./tfms/core/TFMS_Controller.php */
